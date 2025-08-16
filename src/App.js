6d2518a9a82e09005f074c14b20d6d0734bfb00f
xturus138 <radityaramadhan.138@gmail.com>
2025-08-16 09:21:38 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Compfest!
+          Learn react
         </a>
       </header>
     </div>
