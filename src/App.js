c3451598c0050b27c91e04eb99209277a6853b37
xturus138 <radityaramadhan.138@gmail.com>
2025-08-16 09:15:05 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
+  returnn(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
